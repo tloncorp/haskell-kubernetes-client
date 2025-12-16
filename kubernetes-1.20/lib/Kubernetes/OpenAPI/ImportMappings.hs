aff16e354a162189601474cc06048718ef5f7679
Juan Raphael Diaz Simões <juanrapha@gmail.com>
2025-12-16 16:55:36 +0500

    Delete uneeded versions

--- kubernetes-1.20/lib/Kubernetes/OpenAPI/ImportMappings.hs
+++
@@ -1,6 +0,0 @@
-{-# OPTIONS_GHC -fno-warn-dodgy-imports #-}
-
-module Kubernetes.OpenAPI.ImportMappings (module ImportMappings) where
-
-import Kubernetes.OpenAPI.CustomTypes as ImportMappings (Quantity(..))
-import Kubernetes.OpenAPI.CustomTypes as ImportMappings (IntOrString(..))
